39f8691c13839f84252ee70d5c0a57f487ed3e16
David King <dsmk@bu.edu>
2018-02-14 11:40:46 -0500

    Initial prod configuration with a partial sites.map file

---
+++ README.md
@@ -0,0 +1,125 @@
+# Over view
+
+This repo contains the landscape specific files for the BU web router.  In general updates to this repo are
+safe to release to in a change restriction period.  That is because:
+
+- The version of the base image is date tagged to ensure that no software updates accidently sneak through.
+- The config files in this repo are limited to map files
+- The CodeBuild part of the release process will run nginx -t to ensure that there are no map errors prior
+  to release.
+- The CodePipeline will back out to the old running version if the updated version does not stabilize.
+
+# Operational Tasks
+
+## Changing a map entry
+
+1. Use `git pull` to ensure that we are operating with the latest version of the configuration.
+2. Use `git status` to double-check that you are on the correct landscape.  If not then do `git checkout landscape`
+3. Edit landscape/LANDSCAPE/maps/sites.map to reflect our map path.
+4. Use `git status` to double-check status of files.
+5. Use `git diff` to see what changed.
+6. Use `git add` to select files for inclusion.
+7. Use `git commit -m "description" ` to commit the change with the description of the change
+8. Use `git push` to push these changes out to the GitHub repo.  This will trigger the CodePipeline update.
+9. Watch the CodePipeline release through the web console or cli for any issues.
+
+## Updating to a newer base image
+
+During initial builds and configuration we are using the tag latest.
+
+1. Use `git pull` to ensure that we are operating with the latest version of the configuration.
+2. Use `git status` to double-check status of files.
+3. Edit Dockerfile and change the tag as part of the `FROM dsmk/web-router-base:date`
+4. Use `git status` to double-check status of files.
+5. Use `git diff` to see what changed.
+6. Use `git add` to select files for inclusion.
+7. Use `git commit -m "description" ` to commit the change with the description of the change
+8. Use `git push` to push these changes out to the GitHub repo.  This will trigger the CodePipeline update.
+9. Watch the CodePipeline release through the web console or cli for any issues.
+
+## Adding a new landscape
+
+1. Clone a new directory with the landscape name `git clone git@github.com:dsmk/web-router-nonprod.git test`
+2. Cd into the directory and create and checkout the new branch: `cd test; git checkout -b test`
+3. Create the landscape specific subdirectory: `mkdir -p landscape/test/maps`
+4. Copy initial files from a previous landscape except for the sites.map which comes from landscape/common/sites-common.map
+5. Edit files as appropriate.
+6. For sites.map, use PHPMAP and the filesystem scan to find things.
+
+
+This repo has all the data for the BU AWS web front-end proof of concept.  The architecture sets up a fairly 
+standard AWS Elastic Container Service (ECS) environment with CloudFront in front of it.  The CloudFormation 
+templates are in the aws/ subdirectory but eventually should be in a separate location.
+
+This is still a work in progress and we still need to address the following issues:
+
+- Patching model for ECS EC2 instances: ECS does not seem to handle this by default.  Is the correct approach
+  to use EC2 Systems Manager?
+
+- Method of updating route configuration: This version bakes the routing into the Docker image and relies upon
+  building a new Docker image when we change the configuration.  The CodePipeline makes that more straightforward
+  but a nice to have would be a simple interface to adjust the change as long as it does not add much complexity 
+  to the solution.  
+
+- Release mechanism for multiple landscapes: This POC code only handles one landscape (test).  I have seen a
+  different approaches to multiple landscapes:  1) Have a single CodePipeline for multiple landscapes with a 
+  manual approval step prior to production; 2) Have multiple CodePipelines like the POC based on different 
+  repos and/or tags.  
+
+- Approach for handling redirection of both entire subdirectories and singleton URLs: Redirection ideally 
+  should be managed by Help Center folks.  I have seen a few references to lambda/API gateway and S3 bucket 
+  based approaches.  If we use those solutions then hopefully the routing table could just have a "redirect" 
+  backend that points to the solution.
+
+- Internal load balancers for backends: Our existing front-ends handle load balancing for the WordPress and 
+  Django backends (mod_proxy_balancer Apache 2.2).  One refinement would be to have internal application load
+  balancers so all load balancing is handled (and monitored/tracked) by AWS load balancing services.  In 
+  addition, the WordPress ALB could potentially handle selecting between Application and Asset servers which 
+  would simplify this NGINX/HTTPD configuration even more. 
+
+The general workflow while we are building the system is to:
+
+1) Update the test/sut/test_app.sh script to test the new functionality with the current Solaris.  This involves 
+   running the script like so `./test/sut/test_app.sh -S test www-test.bu.edu`  - the -S disables the check for 
+   upstream headers (only configured in NGINX for now).
+
+2) Once that is done then one needs to update the NGINX configuration to implement the same functionality as 
+   Solaris.  This mainly will involve the \*.conf.erb files in the main directory but make involve new environment
+   entries.  One can test this on a local build box by running `docker-compose -f www-test.bu.edu.yaml up --build` 
+   and then `./test/sut/test_app.sh test`  This is testing with the normal www-test backends.
+
+3) Next the automatic testing needs to be configured.  This involves two sets of changes - `autotest-test.yml` 
+   docker-compose file and updates to the `test/backend` Docker image.  The docker-compose file will create a
+   container to run the tests, an NGINX frontend container, and a test backend container which emulates test
+   responses.  The goal of this test framework is to test and minimize regressions in the frontend containers.  It
+   is not intended to be a test of whole services.
+
+4) One can pretest the automatic testing by doing `docker-compose -f autotest-test.yaml up --build ` - if 
+   everything is OK then the last line will end "exited with code 0."  Control-C to exit from the test and 
+   it will stop all the containers set up.  The test output is stored in the `test/results/` directory and you 
+   can see the specific test that failed by searching for `exit code: 1` 
+
+5) When you are ready to release changes you need to check the changes into Git and push them to GitHub.  Here 
+   are some common commands:
+        a) `git status` - see which files have been modified, which are new, what is selected for commiting, and 
+           the branch.
+        b) `git diff [file]` - see what has changed from the last commit.
+        c) `git add file` - add a file to the list of things to check in.
+        d) `git commit -m 'msg'` - commit added files with the message msg (include INC* and ENC* where appropriate)
+        e) `git push` - push the changes to github
+
+6) Right now one needs to go to hub.docker.com/r/dsmk/bufe-buedu, select "Build Settings", and select the Trigger
+   to start the build.  This will be replaced later.  You can see the status of the build by going to "Build Details"
+
+7) Once the new image is done you can replace the existing container by going to the ECS web console, selecting
+   the task we want to modify (bufe-bu.edu-bufe:num), select "Create new revision", and then selecting "Create".  
+   This will take a while to run because ECS will create a new container, add it to the ELB/ALB, and wait for the 
+   old one to drain completely before being done.  This will be replaced later.
+
+URLs for this process:
+- https://sanderknape.com/2016/06/getting-ssl-labs-rating-nginx/
+- https://sanderknape.com/2017/08/custom-cloudformation-resource-example-codedeploy/?__s=1qw3d4dssvntsxck5tuk
+- https://aws.amazon.com/blogs/compute/managing-secrets-for-amazon-ecs-applications-using-parameter-store-and-iam-roles-for-tasks/
+
+- http://blog.xebia.com/docker-container-secrets-aws-ecs/
+
---
+++ test_router.sh
@@ -0,0 +1,283 @@
+#!/bin/sh 
+# 
+# Simple script to use curl to test an app node prior to switching over
+#
+export http_proxy
+http_proxy=
+export https_proxy
+https_proxy=
+
+julog=/bin/false
+do_upstream=/bin/true
+exitcode=0
+
+host=www-devl.bu.edu
+
+test_log () {
+  label="$1"
+  shift
+  status="$1"
+  shift
+  log="$1"
+
+  if $julog ; then
+    if juLog -name="$label" "$status" "$log" ; then
+      /bin/true
+    else
+      exitcode=1
+    fi
+  else
+    if "$status" ; then
+      echo "PASS $label $log"
+    else
+      echo "FAIL $label $log"
+      exitcode=1
+    fi
+  fi
+}
+
+# test_url label url host retcode upstream strings...
+#
+test_url () {
+
+  if [ "x$1" = "x--head" ]; then
+    CURL_ARGS="-Is"
+    shift
+  else
+    CURL_ARGS="-is"
+  fi
+  if [ "x$1" = "x--referer" ]; then 
+    shift
+    CURL_ARGS="$CURL_ARGS -H 'Referer:$1'"
+    shift
+  fi
+  label="$1"
+  shift
+  url="$1"
+  shift
+  #host="$1"
+  #shift
+  retcode="$1"
+  shift
+  upstream="$1"
+  shift
+
+  # if we have a global limit then see if this 
+  skip=/bin/false
+  if [ "x$limit" != "xall" ]; then
+    if echo "$label" | grep -q -v "$limit" ; then
+      skip=/bin/true
+    fi
+  fi
+  if $skip ; then
+    return
+  fi
+
+  tmp_file="/tmp/test_url.$$"
+
+  # ####
+  # first, try the URL storing the output in a file
+  # want -k option so SSL cert will work
+  #
+  if $verbose ; then
+    echo "curl $CURL_ARGS -k -H Host:$HOST $url"
+  fi
+  if curl $CURL_ARGS -k -o "$tmp_file" -H "Host:$HOST" "$url" ; then
+    # worked so do our tests
+    # on sucessful runs we only want to print one line for both upstream and return code
+    pass_ret_and_upstream=/bin/true
+    # return code:
+    t_retcode=$(head -1 "$tmp_file" | awk '{print $2}' )
+    if [ "x$t_retcode" = "x$retcode" ]; then
+      /bin/true; #echo "PASS $url retcode=$retcode"
+    else
+      test_log "$label" /bin/false "retcode=$t_retcode instead of $retcode"
+      pass_ret_and_upstream=/bin/false
+    fi
+
+    # look for the upstream provider field
+    if $do_upstream ; then
+      t_upstream=$(grep '^X-Upstream: ' "$tmp_file" | tr -d '\r' | awk '{print $2}' )
+      #echo "upstream=$upstream t_upstream=$t_upstream"
+      if [ "x$t_upstream" = "x$upstream" ] ; then
+        /bin/true; #echo "PASS $label upstream=$upstream"
+      else
+        test_log "$label" /bin/false "upstream=$t_upstream instead of $upstream"
+        pass_ret_and_upstream=/bin/false
+      fi
+    else
+      upstream="(skipped)"
+    fi
+
+    if $pass_ret_and_upstream ; then
+      test_log "$label" /bin/true "retcode=$retcode upstream=$upstream"
+    fi
+ 
+    # now regexp to be found in the output
+    for regexp in "$@" ; do
+      if grep -q "$regexp" "$tmp_file" ; then
+        test_log "$label" /bin/true "regexp ($regexp) found"
+      else
+        test_log "$label" /bin/false "regexp ($regexp) NOT found"
+      fi 
+    done 
+  else
+    # error occured
+    test_log "$label" /bin/false "**** could not access URL"
+  fi
+
+  if [ -f "$tmp_file" ]; then
+    if $debug ; then
+      cat "$tmp_file"
+    fi
+    rm "$tmp_file"
+  fi 
+}
+
+limit="all"
+if [ "x$1" = "x-S" ]; then
+  do_upstream=/bin/false
+  shift
+fi
+if [ "x$1" = "x-j" ]; then
+  source /sh2ju.sh
+  juLogClean
+  julog=/bin/true
+  shift
+fi
+if [ "x$1" = "x-t" ]; then
+  shift
+  limit="$1"
+  shift
+fi
+debug=/bin/false
+if [ "x$1" = "x-d" ]; then 
+  debug=/bin/true
+  shift
+fi
+verbose=/bin/false
+if [ "x$1" = "x-v" ]; then 
+  verbose=/bin/true
+  shift
+fi
+# now determine landscape and hostname from role and landscape in hostname
+# From the 3rd part of the host name, obtain host role (e.g., "content" or "app"):
+# where host name is like "ist-uiscgi-app-syst01", where role is "app".
+#system_host=`/bin/hostname`
+#ROLE=`echo $system_host | /bin/sed -n -e 's/^[^-]*-[^-]*-\([^-]*\)-.*$/\1/p'`;
+#LANDSCAPE=`echo $system_host | /bin/sed -n -e 's/.*[^-][^-]*-\([A-Za-z][A-Za-z]*\)[0-9][0-9][.]*.*$/\1/p'`;
+
+#if [ "x$ROLE" = "xapppci" ]; then
+#  PREFIX="wwws"
+#else
+#  PREFIX="www"
+#fi
+LANDSCAPE="$1"
+if [ "x$LANDSCAPE" = x ]; then
+  echo "$0 landscape [host]"
+  echo "landscape=syst,devl,buwd,qa,test,prod"
+  exit
+fi
+# if we have another parameter that will be the hostname to use on the requests
+CONNECT_TO="localhost"
+if [ "x$2" != "x" ]; then
+  CONNECT_TO="$2"
+fi
+
+if [ "$LANDSCAPE" = "prod" ]; then
+  HOST="www.bu.edu"
+else
+  HOST="www-$LANDSCAPE.bu.edu"
+fi
+
+if $julog ; then
+  /wait-for-it.sh -t 60 "$CONNECT_TO:80"
+fi
+
+echo ""
+echo "### Test default routing to WordPress"
+test_url "default-wpapp-http-admissions" http://${CONNECT_TO}/admissions/ 200 wordpress
+test_url "default-wpapp-https-admissions" https://${CONNECT_TO}/admissions/ 200 wordpress
+if [ "$LANDSCAPE" != "syst" ]; then 
+  test_url --head  "default-wpassets-http-admissions" http://${CONNECT_TO}/admissions/files/2012/09/footbar_visit.png 200 wpassets
+  test_url "default-wpapp-http-admissions" http://${CONNECT_TO}/admissions/visit-us/schedule-your-visit/ 200 wordpress
+fi
+
+echo ""
+echo "### Test legacy routing"
+test_url "legacy-http-htbin-weblogin" http://${CONNECT_TO}/htbin/login 302 legacy "bu.edu/web@login3/"
+test_url "legacy-http-web3270" http://${CONNECT_TO}/uis_web3270/ 302 legacy "bu.edu/web@login3/"
+
+echo ""
+echo "### Test routing to UISCGI"
+test_url "uiscgi-http-link-trailing-slash" http://${CONNECT_TO}/link 301 uiscgi_content "Location: http://$HOST/link/"
+test_url "uiscgi-http-link-to-uiscgi" http://${CONNECT_TO}/link/ 302 uiscgi_content "Location: http://$HOST/link/bin/uiscgi.pl/uismpl/menu"
+test_url "uiscgi-http-link-menu" http://${CONNECT_TO}/link/bin/uiscgi.pl/uismpl/menu 200 uiscgi_app "/link/system/images/bu-logo.gif"
+# next test commented out until test backend handles SSL TODO:
+#test_url "uiscgi-https-link-menu" https://${CONNECT_TO}/link/bin/uiscgi.pl/uismpl/menu 200 uiscgi_app "/link/system/images/bu-logo.gif"
+test_url "uiscgi-http-link-args" http://${CONNECT_TO}/link/bin/args.pl 200 uiscgi_app "Cache-Control: no-cache"
+# next test commented out until test backend handles SSL TODO:
+#test_url "uiscgi-https-link-args" https://${CONNECT_TO}/link/bin/args.pl 200 uiscgi_app "Cache-Control: no-cache"
+#test_url https://localhost/link/bin/uiscgi.pl  200 uiscgi "/link/system/images/bu-logo.gif"
+
+# test mainframe CUSSP call
+echo ""
+echo "### Test studentlink and mainframe CUSSP call"
+test_url "uiscgi-http-studentlink-redirect" "http://${CONNECT_TO}/studentlink/" 302 redirect "Location: http://$HOST/link/bin/uiscgi_studentlink.pl/uismpl/?ModuleName=menu.pl&NewMenu=Home"
+test_url "uiscgi-http-studentlink-menu" "http://${CONNECT_TO}/link/bin/uiscgi_studentlink.pl/1486741068?ModuleName=univschr.pl&SearchOptionDesc=Distance+Education&SearchOptionCd=D&KeySem=20183&ViewSem=Fall+2017" 200 uiscgi_app '<TH>Search by:</TH>'
+
+if [ "$LANDSCAPE" != "syst" ]; then
+echo ""
+echo "### Test PHP and Django services"
+test_url "dbin-http-oittest" http://${CONNECT_TO}/dbin/oittest/info.php 200 dbin '<h2>PHP License</h2>'
+test_url "phpbin-http-buniverse" http://${CONNECT_TO}/buniverse/ 200 phpbin buniverse
+test_url "django-http-cellphone" http://${CONNECT_TO}/cellphone-compliance/ 302 django "Location: http://$HOST/cellphone-compliance/accounts/login"
+test_url "django-https-cellphone" https://${CONNECT_TO}/cellphone-compliance/ 302 django "Location: https://$HOST/cellphone-compliance/accounts/login"
+test_url "django-http-cwaremgr" http://${CONNECT_TO}/courseware-manager/admin/ 302 django "Location: http://$HOST/courseware-manager/admin/login/"
+test_url "django-http-dnamixtures" http://${CONNECT_TO}/dnamixtures/ 200 django /dnamixtures/static/dna_lessons
+fi
+test_url "phpbin-http-maps" http://${CONNECT_TO}/maps/ 200 phpbin 'var BUMapsVersion = 1'
+
+echo ""
+echo "Robots.txt"
+if [ "$LANDSCAPE" = "prod" ]; then
+  test_url "robots-http-prod" "http://${CONNECT_TO}/robots.txt" 200 content 'default action - currently it allows access to most of the site'
+else
+  test_url "robots-http-non-prod" "http://${CONNECT_TO}/robots.txt" 200 content 'disallows all robots on test and development servers'
+fi
+test_url "homepage-http-content" "http://${CONNECT_TO}/" 200 content '<!-- HPPUBDDATE'
+
+if false ; then
+echo ""
+echo "Testing redirects"
+test_url "redirect-http-law-foo" "http://${CONNECT_TO}/LAW/foo" 301 redirect 'Location: http://www.bu.edu/law/foo'
+test_url "redirect-http-law-foo" "http://${CONNECT_TO}/LAW" 301 redirect 'Location: http://www.bu.edu/law'
+test_url "redirect-http-law-foo" "http://${CONNECT_TO}/LAW/foo/bar" 301 redirect 'Location: http://www.bu.edu/law/foo/bar'
+fi
+
+#echo "" 
+#echo "Testing system urls"
+#test_url "server-http-version" "http://${CONNECT_TO}/server/version" 200 version 'hostname: '
+#test_url "server-http-cloudfrontips" "http://${CONNECT_TO}/server/cloudfront_ips" 200 cloudfront_ips 
+
+
+if [ "$LANDSCAPE" != "syst" ]; then
+test_url "http-students" "http://${CONNECT_TO}/students/" 200 wordpress 'generated in'
+
+test_url "http-degree-advice-root" "http://${CONNECT_TO}/degree-advice/" 301 degree-advice "://$HOST/degree-advice/IRISLink.cgi"
+test_url "http-degree-advice-cgi" "http://${CONNECT_TO}/degree-advice/IRISLink.cgi" 302 degree-advice "idp/profile/SAML2/Redirect/SSO"
+test_url "https-degree-advice-cgi" "https://${CONNECT_TO}/degree-advice/IRISLink.cgi" 302 degree-advice "idp/profile/SAML2/Redirect/SSO"
+fi
+
+test_url "http-healthcheck" "http://${CONNECT_TO}/server/healthcheck" 200  healthcheck OK
+test_url "https-healthcheck" "https://${CONNECT_TO}/server/healthcheck" 200  healthcheck OK
+
+#echo ""
+#echo "Testing legacy entries from the Apache config"
+#test_url "apache-redirect-pcsc" "http://${CONNECT_TO}/pcsc/foo" 301 content '/tech/help/foo'
+
+# to double-check stuff
+#debug=/bin/true
+#test_url "https-degree-advice-cgi" "https://${CONNECT_TO}/degree-advice/IRISLink.cgi" 302 degree-advice "idp/profile/SAML2/Redirect/SSO" "X-Backend-Protocol: http" "X-Backend-Host: test-backend"
+
+exit $exitcode
